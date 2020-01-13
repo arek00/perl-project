eb58c2d3dc24c7c88300b4f6fdea64d430cb052f
arek00 <arek00@yahoo.pl>
2020-01-13 02:07:48 +0100

    Added printing tables to string with Text::Table

--- projekt.pl
+++ projekt.pl
@@ -1,68 +1,95 @@
 #!/usr/bin/perl
 
+### Import libraries
 use 5.010;
 use strict;
 use warnings;
 use DateTime::Format::Strptime;
 use Text::CSV_XS qw ( csv );
+use Text::Table;
 
+### initialize parameters
 my $inputFile = $ARGV[0];
 my @lines = readLines($inputFile);
 my $argsNumber = scalar @ARGV;
 my @searchingPatterns = @ARGV[1..$argsNumber-1];
 
-say("Used patterns");                         
-foreach my $pattern (@searchingPatterns) {
-    print($pattern."\n");
-}
 
+### Generate report 1
+say("Start generating report 1");
 my @report1Data = generateReport1Data(@lines, @searchingPatterns);
 @report1Data = parseDates(@report1Data);
+
+say("Sorting data in report 1");
 @report1Data = sort {$a->{data} cmp $b->{data}} @report1Data;
+
+say("Convert dates to string in report 1");
 @report1Data = datesToString(@report1Data);
 
+### Printing report 1
+say("Create table 1");
+my $table = Text::Table->
+    new(columnTitle("Data"), columnTitle("Autor"), columnTitle("Czasopismo"));
+foreach my $reportRow(@report1Data) {
+    my %row = %$reportRow;
+    $table->load([$row{data}, $row{autor}, $row{gazeta}]);
+}
 
+my $report1File = "raport1.txt";
+print($table);
+open(my $fileHandler, '>', $report1File) or die;
+print $fileHandler "Słowa kluczowe: ".arrayToStr(@searchingPatterns)."\n";
+print $fileHandler $table;
+
+
+### Generate report 2
+say("Start generating report 2");
 my @report2Data = generateReport2Data();
-#@report2Data = parseDates(@report2Data);
-#@report2Data = sort compareArraysLengths @report2Data;
-#@report2Data = datesToString(@report2Data); 
-#@report2Data = map {$_{matched} = arrayToStr($_{matched})} @report2Data;
+
+say("Parsing dates in report 2");
+@report2Data = parseDates(@report2Data);
 
 
-say("Mapped dates:");
-say("Data"
-    ."\t"
-    ."Autor"
-    ."\t"
-    ."Czasopismo");
-foreach my $record (@report1Data) {
-    my %recordHash = %$record;
-    
-    say($recordHash{data}
-        ."\t"
-        .$recordHash{autor}
-        ."\t"
-        .$recordHash{gazeta});
+say("Sorting data in report 2");
+@report2Data = sort {scalar(@{$b->{matched}}) cmp scalar(@{$a->{matched}})} @report2Data;
+
+say("Mapping arrays to strings in report 2");
+@report2Data = mapMatchedWordsToString(@report2Data);
+
+say("Creting report 2 table");
+my $reportTable2 = Text::Table->
+    new(columnTitle("Data"), columnTitle("Autor"), columnTitle("Czasopismo"), columnTitle("Wyszukane słowa"));
+
+say("Filling report 2 table with data");
+foreach my $reportRow (@report2Data) {
+    my %row = %$reportRow;
+    $reportTable2->load([$row{data}, $row{autor}, $row{gazeta}, arrayToStr(@{$row{matched}})]);
 }
 
 
+say("Writing report 2 to file");
+my $report2File = "raport2.txt";
+print($reportTable2);
+open(my $fileHandler2, '>', $report2File) or die;
+print $fileHandler2 "Słowa kluczowe: ".arrayToStr(@searchingPatterns)."\n";
+print $fileHandler2 $reportTable2;
 
+#################################################
 
-say("Report 2 output");
-foreach my $record (@report2Data) {
-    my %recordHash = %$record;
+### Functions definitions
+
+sub columnTitle {
+    say("Start column title");
+
+    my $string = $_[0];  
+    my $strLen = length($string);    
+    my $line = "-" x $strLen;
     
-    say($recordHash{data}
-        ."\t"
-        .$recordHash{autor}
-        ."\t"
-       .join(", ", @recordHash{matched}));
+    return "$line\n$string\n$line";
 }
-
-
-say(@report2Data[0]->{matched});
 
 sub readLines {
+    say("Start read lines");
     my $filePath = $_[0];
     my $parsedCsv = csv (in => $filePath,
                         headers => "auto",
@@ -71,15 +98,14 @@ sub readLines {
 }
 
 sub generateReport1Data {
+    say("Start generating report 1");
     my @results = ();
     foreach my $line (@lines) {
         my %_line = %$line;
-       # print(%line);        
     
         foreach(@searchingPatterns) {
             my $pattern = $_;
             if($_line{tekst} =~ m/$pattern/i) {
-                print($_line{autor}."\n");
                 push(@results, \%_line);
                 last;
             }
@@ -90,6 +116,8 @@ sub generateReport1Data {
 }
 
 sub generateReport2Data {
+    say("Start generating report 2");
+
     my @results = ();
     
     ## Iterate over each CSV' file line.
@@ -98,34 +126,42 @@ sub generateReport2Data {
         
         my @words = split(/\s+/, $_line{tekst});
         @words = map {s/[\"\,\.]//; $_} @words;
+        my @matchedWords = ();
         
-        my @matchedWords = ();       
+        my $wordsInLine = arrayToStr(@words);
+        say("Processing text: $wordsInLine");       
         
         ## Iterate over each word from tekst column.
         foreach my $word (@words) {
-            ##say ("Matching word ".$word);
             ## Iterate over each defined word/pattern to match to find if any pattern matches word.
-            foreach(@searchingPatterns) {
-                my $pattern = qr/$_/;
-                if($word =~ m/[^\s\,\.]*$pattern[^\s\,\.]*/i) {
+            
+            #say("Matching word $word");
+            
+            foreach my $searchingPattern (@searchingPatterns) {
+                my $pattern = qr/$searchingPattern/i;
+                #say("Attempting to match $word with pattern $searchingPattern");
+                if($word =~ m/\w*$pattern\w*/i) {
                     push(@matchedWords, $word);
-                    
-                    say("Matched $pattern to $word");
+                    #say("Matched $searchingPattern to $word");
                     last;
                 }
-            }
-            
-            #say("Matched words");
-            #say(@matchedWords);
-            $_line{matched} = @matchedWords;
+            }                       
+        }
+        
+        if(scalar(@matchedWords) > 0) {
+            my $matchedStr = arrayToStr(@matchedWords);
+            say("Matched words: $matchedStr");
+            $_line{matched} = \@matchedWords;        
             push(@results, \%_line);
-        }   
+        }
     }
     
     return @results;
 }
 
 sub parseDate {
+    say("Start parse date");
+
     my $date = $_[0];
     #say("Date to parse: ".$date);
     
@@ -140,7 +176,7 @@ sub parseDate {
 }
 
 sub dateToString {
-    #print($_[0]);
+    say("Start date to string");
     my $dateStringPattern = "%d.%m.%Y";
     my $date = $_[0];
     my $strDateTime = DateTime::Format::Strptime::strftime($dateStringPattern, $date) or die;
@@ -148,6 +184,7 @@ sub dateToString {
 }
 
 sub parseDates {
+    say("Start parse dates");
     my @array = @_;
     
     my @ret = ();
@@ -177,16 +214,8 @@ sub datesToString {
     return @ret;
 }
 
-sub report1 {
-    my @_searchingPatterns = @_;
-
-    print("Słowa kluczowe: ");
-    my $patternsJoined = join(" lub ", @_searchingPatterns);
-    print($patternsJoined); 
-
-}
-
 sub compareArraysLengths {
+    say("Start compare array lengths");
     my %hash1 = %$a;
     my %hash2 = %$b;
 
@@ -198,7 +227,19 @@ sub compareArraysLengths {
     return @array1.length() - @array2.length();
 }
 
+sub mapMatchedWordsToString {
+    my @array = @_;
+    
+    foreach my $line (@array) {
+        my %lineHash = %$line;
+        $lineHash{matchedStr} = arrayToStr(%lineHash{matched});
+    }
+    
+    return @array;
+}
+
 sub arrayToStr {
-    my @array = $_[0]; 
-    return join(", ", @array);     
+    say("Start array to string");
+    my @array = @_; 
+    return join(",", @array);     
 }
